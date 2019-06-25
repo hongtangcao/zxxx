97a6d9c178e59dde97db01e24fb3b876a50386b9
曹洪堂 <11318230@qq.com>
2019-06-26 00:41:08 +0800

    上传文件

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,6 @@
 <!--检测鼠标滚动-->
 $(document).scroll(function () {
     var scroll = $(document).scrollTop();
-    console.log(scroll);
     if(scroll>42){
         $(".head").css({"background-color":"rgba(255,255,255,.95)"});
     }
